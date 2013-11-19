9207647e4f07004cf73e3fd9c33fc78c71261514
Kerry Buckley <kerryjbuckley@gmail.com>
2013-11-19 12:40:45 +0000

    Gutter game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,12 @@
 require "game"
 
 describe Game do
+  subject(:game) { Game.new }
+
+  describe "A gutter game" do
+    it "scores zero" do
+      20.times { game.roll 0 }
+      expect(game.score).to be_zero
+    end
+  end
 end
